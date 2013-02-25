8e1d740a30fb5b707f7610ae5d9d6a1a70eeab57
fmw-jk <jannik.kett@googlemail.com>
2013-02-25 16:16:03 +0100

    global pipeline configs

--- src/main/java/de/fraunhofer/ipa/CobPipelineProperty.java
+++ src/main/java/de/fraunhofer/ipa/CobPipelineProperty.java
@@ -65,17 +65,17 @@ import org.kohsuke.stapler.QueryParameter;
  */
 public class CobPipelineProperty extends UserProperty {
 	
-	/*
+	/**
 	 * user email address
 	 */
 	private String email = null;
 	
-	/*
+	/**
 	 * user name
 	 */
 	private String userName = null;
 	
-	/*
+	/**
 	 * Jenkins master name
 	 */
 	private String masterName = null;
@@ -135,6 +135,12 @@ public class CobPipelineProperty extends UserProperty {
 	@Extension
     public static class DescriptorImpl extends UserPropertyDescriptor {
 
+		private String githubAdmin;
+		
+		public DescriptorImpl() {
+			load();
+		}
+		
         @Override
         public String getDisplayName() {
             return "Pipeline Configurations";
@@ -145,7 +151,11 @@ public class CobPipelineProperty extends UserProperty {
             return new CobPipelineProperty(user.getId());
         }
         
-        public FormValidation doCheckEmail(@QueryParameter String value) throws IOException, ServletException {
+        /**
+         * Checks if the given String is a email address
+         */
+        public FormValidation doCheckEmail(@QueryParameter String value)
+        		throws IOException, ServletException {
         	if (value.equals("enter your email address here")) {
         		return FormValidation.warning("email address not set yet");
         	}
@@ -157,6 +167,33 @@ public class CobPipelineProperty extends UserProperty {
     			return FormValidation.error("invalid email address");
     		}
     	}
+        
+        public void setGithubAdmin(String githubAdmin) {
+        	this.githubAdmin = githubAdmin;
+        }
+        
+        public String getGithubAdmin() {
+        	return githubAdmin;
+        }
+        
+        /**
+         * Checks if given String is valid GitHub user
+         */
+        public FormValidation doCheckGithubAdmin(@QueryParameter String value)
+        		throws IOException, ServletException {
+        	if (value.length() == 0) {
+        		return FormValidation.error("Please enter name");
+        	}
+        	//TODO implement
+        	return FormValidation.ok();
+        }
+        
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject form) throws FormException {
+        	req.bindJSON(this, form);
+        	super.save();
+        	return super.configure(req, form);
+        }
     }
     
     @Override
